e91d04861493c87c3e58800c4aa80e7681147b05
Weifan Lee <weifan@ebmtech.com>
2017-08-30 02:33:24 +0800

    第一題   測試建立 : 第一集買了一本，其他都沒買，價格應為100*1=100元

---
+++ HarryPotterShop/HarryPotterShopLib/HarryPotterShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotterShopLib
+{
+    public class HarryPotterShoppingCart
+    {
+        public HarryPotterShoppingCart()
+        {
+        }
+
+        public void AddProductInCart(HarryPotterProduct product)
+        {
+        }
+
+        public double CheckOut()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HarryPotterShop/HarryPotterShop_Test/HarryPotterShop_Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HarryPotterShopLib;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HarryPotterShop_Test
+{
+    [TestClass]
+    public class HarryPotterShop_Test
+    {
+        [TestMethod]
+        public void Test_CartCheckout_Only_Buy_1_booK_with_FirstEpisode()
+        {
+            //arrange
+            var cart = new HarryPotterShopLib.HarryPotterShoppingCart();
+
+            //act
+            var product = GetAllProduct().Where(x => x.ProductID == "P01").FirstOrDefault();
+            cart.AddProductInCart(product);
+            var actual = cart.CheckOut();
+
+            //accert
+            double expected = 100;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        private IEnumerable<HarryPotterProduct> GetAllProduct()
+        {
+            var allProduct = new List<HarryPotterProduct>() {
+            new HarryPotterProduct { ProductID = "P01", ProductName = "哈利波特第一集", SellPrice = 100 },
+            new HarryPotterProduct { ProductID = "P02", ProductName = "哈利波特第二集", SellPrice = 100 },
+            new HarryPotterProduct { ProductID = "P03", ProductName = "哈利波特第三集", SellPrice = 100 },
+            new HarryPotterProduct { ProductID = "P04", ProductName = "哈利波特第四集", SellPrice = 100 },
+            new HarryPotterProduct { ProductID = "P05", ProductName = "哈利波特第五集", SellPrice = 100 }
+            };
+            return allProduct;
+        }
+    }
+}
